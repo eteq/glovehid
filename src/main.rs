91717bc03222d725199cf1c4861d9eb2a61c9b05
Erik Tollerud <erik.tollerud@gmail.com>
2022-01-26 02:57:16 -0500

    generified SPI

--- src/main.rs
+++ src/main.rs
@@ -34,9 +34,6 @@ fn main() -> ! {
     
     let sw2 = port0.p0_29.into_pullup_input();
 
-     //set_dotstar_color(0.5, 0.2, 0.3, 0.4, 
-      //                 &mut dot_data.degrade(), &mut dot_clk.degrade());
-
     let pins = hal::spim::Pins {
         sck: dot_clk.degrade(),
         mosi: Some(dot_data.degrade()),
@@ -74,100 +71,8 @@ fn main() -> ! {
     }
 }
 
-fn set_dotstar_color(r:f32, g:f32, b:f32, brightness:f32, 
-                     data_pin: &mut Pin<Output<PushPull>>, 
-                     clock_pin: &mut Pin<Output<PushPull>>) {
-    // not sure if this is necesary but it's in the timing diagram..
-    clock_pin.set_high().unwrap();
-
-    let rbyte = (r * 255.) as u8;
-    let gbyte = (g * 255.) as u8;
-    let bbyte = (b * 255.) as u8;
-    let brightnessbyte = (brightness * 32.) as u8;
-    
-    cortex_m::interrupt::free(|_| {
-        // 32 bits of 1
-        data_pin.set_low().unwrap();
-        for _ in 0..32 {
-            clock_pin.set_low().unwrap();
-            cortex_m::asm::delay(8_u32);
-            clock_pin.set_high().unwrap();
-            cortex_m::asm::delay(8_u32);
-        }
-
-        // leading 3 bits
-        data_pin.set_high().unwrap();
-        for _ in 0..3 {
-            clock_pin.set_low().unwrap();
-            cortex_m::asm::delay(8_u32);
-            clock_pin.set_high().unwrap();
-            cortex_m::asm::delay(8_u32);
-        }
-
-        // brightness
-        for i in 0..5 {
-
-            if (brightnessbyte << i) & 0b10000 == 0 
-            { data_pin.set_low().unwrap(); }
-            else {  data_pin.set_low().unwrap(); }
-
-            clock_pin.set_low().unwrap();
-            cortex_m::asm::delay(8_u32);
-            clock_pin.set_high().unwrap();
-            cortex_m::asm::delay(8_u32);
-        }
-
-        // r
-        for i in 0..8 {
-            if (rbyte << i) & 0b10000000 == 0 
-            { data_pin.set_low().unwrap(); }
-            else {  data_pin.set_low().unwrap(); }
-
-            clock_pin.set_low().unwrap();
-            cortex_m::asm::delay(8_u32);
-            clock_pin.set_high().unwrap();
-            cortex_m::asm::delay(8_u32);
-        }
-
-        // g
-        for i in 0..8 {
-            if (gbyte << i) & 0b10000000 == 0 
-            { data_pin.set_low().unwrap(); }
-            else {  data_pin.set_low().unwrap(); }
-
-            clock_pin.set_low().unwrap();
-            cortex_m::asm::delay(8_u32);
-            clock_pin.set_high().unwrap();
-            cortex_m::asm::delay(8_u32);
-        }
-
-        // b
-        for i in 0..8 {
-            if (bbyte << i) & 0b10000000 == 0 
-            { data_pin.set_low().unwrap(); }
-            else {  data_pin.set_low().unwrap(); }
-
-            clock_pin.set_low().unwrap();
-            cortex_m::asm::delay(8_u32);
-            clock_pin.set_high().unwrap();
-            cortex_m::asm::delay(8_u32);
-        }
-
-        
-        // 32 bits of 1
-        data_pin.set_high().unwrap();
-        for _ in 0..32 {
-            clock_pin.set_low().unwrap();
-            cortex_m::asm::delay(8_u32);
-            clock_pin.set_high().unwrap();
-            cortex_m::asm::delay(8_u32);
-        }
-        clock_pin.set_low().unwrap();
-    });
-}
-
-fn set_dotstar_color_spi(r:f32, g:f32, b:f32, brightness:f32, 
-                            spim: &mut hal::Spim<hal::pac::SPIM0>,
+fn set_dotstar_color_spi<T:hal::spim::Instance>(r:f32, g:f32, b:f32, brightness:f32, 
+                            spim: &mut hal::Spim<T>,
                             cs: &mut Pin<Output<PushPull>>) {
     let rbyte = (r * 255.) as u8;
     let gbyte = (g * 255.) as u8;
