14a185110191ce360c68c15293d50b386b411bfe
Erik Tollerud <erik.tollerud@gmail.com>
2022-01-26 22:14:18 -0500

    bit-bang dotstar

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ use cortex_m_rt::entry;
 use nrf52840_hal as hal;
 use nrf52840_hal::prelude::*;
 use nrf52840_hal::gpio::{Pin, Output, PushPull, Level};
-//use cortex_m::peripheral::SYST;
 
 #[entry]
 fn main() -> ! {
@@ -26,31 +25,15 @@ fn main() -> ! {
 
     let mut led = port0.p0_06.into_push_pull_output(Level::Low);
     
-    let dot_data = port0.p0_08.into_push_pull_output(Level::Low);
-    let dot_clk = port1.p1_09.into_push_pull_output(Level::Low);
-
-    let spimiso = port1.p1_01.into_floating_input(); // unused
-    let cs = port1.p1_03.into_push_pull_output(Level::Low); // unused
+    let dot_dat: &mut Pin<Output<PushPull>> = &mut port0.p0_08.into_push_pull_output(Level::Low).degrade();
+    let dot_clk: &mut Pin<Output<PushPull>> = &mut port1.p1_09.into_push_pull_output(Level::Low).degrade();
     
     let sw2 = port0.p0_29.into_pullup_input();
 
-    let pins = hal::spim::Pins {
-        sck: dot_clk.degrade(),
-        mosi: Some(dot_data.degrade()),
-        miso: Some(spimiso.degrade()),
-    };
-    let mut spim = hal::Spim::new(
-        p.SPIM0,
-        pins,
-        hal::spim::Frequency::M4,
-        hal::spim::MODE_0,
-        0,
-    );
-    
 
     let mut color_cycle: usize = 0;
     let mut switch_down = false;
-    let csref: &mut Pin<Output<PushPull>> = &mut cs.degrade();
+
     loop {
         if sw2.is_low().unwrap() {
             switch_down = true;
@@ -64,25 +47,40 @@ fn main() -> ! {
         led.set_low().unwrap();
         delay.delay_ms(50_u16);
         
-        set_dotstar_color_spi(0.5 * (color_cycle%3 ==0) as u8 as f32, 
+        set_dotstar_color(0.5 * (color_cycle%3 ==0) as u8 as f32, 
                               0.5 * (color_cycle%3 ==1) as u8 as f32, 
                               0.5 * (color_cycle%3 ==2) as u8 as f32, 
-                              0.04, &mut spim, csref);
+                              0.04, dot_dat, dot_clk);
     }
 }
 
-fn set_dotstar_color_spi<T:hal::spim::Instance>(r:f32, g:f32, b:f32, brightness:f32, 
-                            spim: &mut hal::Spim<T>,
-                            cs: &mut Pin<Output<PushPull>>) {
+
+fn set_dotstar_color(r:f32, g:f32, b:f32, brightness:f32, 
+    dat: &mut Pin<Output<PushPull>>, clk: &mut Pin<Output<PushPull>>) {
     let rbyte = (r * 255.) as u8;
     let gbyte = (g * 255.) as u8;
     let bbyte = (b * 255.) as u8;
     let firstbyte = ((brightness * 31.) as u8) + 0b1110000;
-
+    
     let txbuffer = [0, 0, 0, 0,
                     firstbyte, bbyte, gbyte, rbyte,
                     0xff, 0xff, 0xff, 0xff
                     ];
-    
-    spim.write(cs, &txbuffer).expect("SPI write failed");
+
+    clk.set_high().ok();
+    cortex_m::asm::delay(200_u32);
+
+
+    for b in txbuffer.iter() {
+        for i in 0..8 {
+            clk.set_low().ok();
+            if 0b10000000 & (b << i) == 0 { 
+                dat.set_low().ok();
+            } else {
+                dat.set_high().ok();
+            }
+            clk.set_high().ok();
+        }
+    }
+    clk.set_low().ok();
 }
